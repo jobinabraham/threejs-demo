4a52466ff3c99cc08b43341493d9c20c0849923a
Jobin Abraham <jobinabraham@Jobins-MacBook-Pro.local>
2022-02-16 11:29:52 +0530

    Added socket io chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,57 @@
-import Example from "@components/Example";
-import type { NextPage } from "next";
+import { useEffect, FormEventHandler, useState } from "react";
+import io from "socket.io-client";
 
-const Home: NextPage = () => {
-  return <Example />;
+export default () => {
+  const socket = io();
+  const [messages, setMessages] = useState<String[]>([]);
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault(); //prevents page reloading
+    const data: any = new FormData(e.target as HTMLFormElement);
+    const chat = data.get("chat");
+    socket.emit("chat message", chat);
+  };
+
+  const attachEvents = () => {
+    socket.on("chat message", (data) => {
+      console.log("chat message", data);
+      console.log("New list ", [...messages, data]);
+      setMessages([...messages, data]);
+    });
+  };
+
+  useEffect(() => {
+    fetch("/api/socketio").finally(() => {
+      socket.on("connect", () => {
+        console.log("connect");
+      });
+
+      socket.on("hello", (data) => {
+        console.log("kello", data);
+      });
+
+      socket.on("a user connected", () => {
+        console.log("a user connected");
+      });
+
+      socket.on("disconnect", () => {
+        console.log("disconnect");
+      });
+      attachEvents();
+    });
+  }, [attachEvents]); // Added [] as useEffect filter so it will be executed only once, when component is mounted
+
+  return (
+    <>
+      <ul id="messages">
+        {messages.map((message, index) => (
+          <li key={index + "" + message.charAt(1)}>{message}</li>
+        ))}
+      </ul>
+      <form onSubmit={onSubmit}>
+        <input autoComplete="off" name="chat" />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
 };
-
-export default Home;
---
+++ socket-test/app.js
@@ -0,0 +1,10 @@
+// const app = require("express")();
+// const http = require("http").Server(app);
+
+// app.get("/", (req, res) => {
+//   res.send("hello world!");
+// });
+
+// http.listen(8000, () => {
+//   console.log("listening on port 8000");
+// });
